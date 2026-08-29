225f1b99bf728f6d3aca5c605e67556b3b01ac2a
sanchezSanjar <sanjar@ajou.ac.kr>
2026-08-30 03:34:14 +0900

    fix:organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,20 @@
 // import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import {Link, Route , Routes} from "react-router-dom";
+import { Route , Routes, useLocation} from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
-  return (
-   <div>
-        <nav>
-          <ul>
-              <li>
-              <Link to="/">HomePage</Link>
-            </li>
-            <li>
-              <Link to="/products">ProductsPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">UserPage</Link>
-            </li>
-          
-          </ul>
-        </nav>
+const location = useLocation();
+console.log("location:", location);
+
+ return(
+  <>      
+    {location.pathname === "/" ?<HomeNavbar /> : <OtherNavbar/>}
      
         <Routes>
           <Route path="/products">
@@ -40,8 +30,10 @@ function App() {
             <HomePage />
           </Route>
         </Routes>
-      </div>
-  );
+      <Footer/>
+    </>
+ );
+
 }
 
 export default App;
