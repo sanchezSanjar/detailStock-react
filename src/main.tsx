f3d8beed4fc124ceb6c3b8ae8d4c6696c8f7b772
sanchezSanjar <sanjar@ajou.ac.kr>
2026-08-30 14:18:04 +0900

    fix:modify root index render logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
@@ -8,7 +8,11 @@ import { ThemeProvider } from "@emotion/react";
 import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
